aff1bc4af9da39fee80356569a5b05c75b899959
Saurabh Joshi <saurabhsjoshi94@gmail.com>
2020-04-11 15:13:00 -0400

    Added simple scaffolding for the view
    - Created home screen widget
    - Added bloc provider
    - Added state check to home screen
    
    Signed-off-by: Saurabh Joshi <saurabhsjoshi94@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:http/http.dart' as http;
+import 'package:weather/AppBlocDelegate.dart';
+import 'package:weather/bloc/bloc.dart';
+import 'package:weather/screen/home/home.dart';
+import 'package:weather/service/service.dart';
 
-void main() => runApp(MyApp());
+import 'repository/repository.dart';
 
-class MyApp extends StatelessWidget {
+void main() {
+  BlocSupervisor.delegate = AppBlocDelegate();
+  final WeatherRepository weatherRepository = WeatherRepository(
+    weatherService: WeatherService(
+      httpClient: http.Client(),
+    ),
+  );
+  runApp(App(
+    weatherRepository: weatherRepository,
+  ));
+}
+
+class App extends StatelessWidget {
+  final WeatherRepository weatherRepository;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Weather',
       theme: ThemeData(
-        primarySwatch: Colors.red,
+        primarySwatch: Colors.orange,
       ),
-      home: Container(),
+      home: BlocProvider(
+        create: (context) => WeatherBloc(weatherRepository: weatherRepository),
+        child: Home(),
+      ),
     );
   }
+
+  App({Key key, @required this.weatherRepository})
+      : assert(weatherRepository != null),
+        super(key: key);
 }
---
+++ lib/screen/home/home.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather/bloc/bloc.dart';
+import 'package:weather/model/model.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: getAppBar(context),
+      body: getContent(context),
+    );
+  }
+
+  /// Builds and returns the app bar
+  Widget getAppBar(BuildContext context) {
+    return AppBar(
+      title: Text('Weather'),
+    );
+  }
+
+  /// Builds the content of the page
+  Widget getContent(BuildContext context) {
+    return Center(
+      child: BlocBuilder<WeatherBloc, WeatherState>(
+        builder: (context, state) {
+          if (state is WeatherEmpty) {
+            return Center(child: Text('Please select a location'));
+          }
+          if (state is WeatherLoading) {
+            return Center(child: CircularProgressIndicator());
+          }
+          if (state is WeatherLoaded) {
+            return getWeatherView(state.weather);
+          }
+          if (state is WeatherError) {
+            return Text(state.message, style: TextStyle(color: Colors.red));
+          }
+          return Container();
+        },
+      ),
+    );
+  }
+
+  /// Get the weather view
+  Widget getWeatherView(Weather weather) {
+    return ListView();
+  }
+}
